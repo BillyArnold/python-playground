8286d2924683c85efd94c1fecb2a6c9ab1594636
Billy Arnold <billyarnoldd@gmail.com>
2024-07-12 17:48:06 +0100

    .

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from math_tester import MathTester
 import numpy as np
+import pandas as pd
 
 def main():
-    test = MathTester(np)
+    test = MathTester(np, pd)
     test.print_values()
+    test.create_dataframe()
 
 if __name__ == "__main__":
     main()
--- math_tester.py
+++ math_tester.py
@@ -1,8 +1,9 @@
 #!/opt/homebrew/bin/python3
 
 class MathTester():
-    def __init__(self, numpy):
+    def __init__(self, numpy, pd):
         self.numpy = numpy
+        self.pd = pd
         self.one_dimensional_array = self.numpy.array([1.2, 2.4, 3.5, 4.7, 6.1, 7.2, 8.3, 9.5])
         self.random_integers_between_50_and_100 = self.numpy.random.randint(low=50, high=101, size=(6,))
         self.sequence_of_integers = self.numpy.arange(5, 12)
@@ -10,3 +11,20 @@ class MathTester():
     def print_values(self):
         print('Testing')
         print(self.random_integers_between_50_and_100)
+        print(self.sequence_of_integers)
+
+    def create_dataframe(self):
+                # Create and populate a 5x2 NumPy array.
+        my_data = self.numpy.array([[0, 3], [10, 7], [20, 9], [30, 14], [40, 15]])
+
+        # Create a Python list that holds the names of the two columns.
+        my_column_names = ['temperature', 'activity']
+
+        # Create a DataFrame.
+        my_dataframe = self.pd.DataFrame(data=my_data, columns=my_column_names)
+
+        #adjustments
+        my_dataframe["adjusted"] = my_dataframe["activity"] + 2
+
+        # Print the entire DataFrame
+        print(my_dataframe)
